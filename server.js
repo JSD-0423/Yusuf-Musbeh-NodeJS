c2160182ae193bb2547f91ca0d6a1df8fdfd0dd4
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-05-21 23:07:25 +0300

    refactor: move constans to variables

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const http = require("http");
 const fs = require("fs/promises");
 const server = http.createServer(handelRequest)
+const PORT=3000;
+const HOST= "localhost";
 
 async function  handelRequest   (request,response)  {
-    let file = await fs.open("request.txt","a");
     let DateObject = new Date(Date.now());
-    file.writeFile(`\n${request.url} -- ${DateObject.getHours().toString()}:${DateObject.getMinutes().toString()}`);
+    await fs.appendFile("request.txt",`\n${request.url} -- ${DateObject.getHours().toString()}:${DateObject.getMinutes().toString()}`);
     response.end("data saved successfully");
 }
 
-server.listen(3000,"localhost",()=>console.log("server now is listening on port 3000"))
+server.listen(PORT,HOST,()=>console.log("server now is listening on port 3000"))
