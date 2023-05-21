e9ef76b57f4ab2c036878ab301599fdcf08164b4
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-05-21 23:01:51 +0300

    feat: save data to reqeust.txt file

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require("http");
+const fs = require("fs/promises");
+const server = http.createServer(handelRequest)
 
-const server = http.createServer((request,response)=>{
-    console.log("request received");
-    response.end("hello world");
-})
+async function  handelRequest   (request,response)  {
+    let file = await fs.open("request.txt","a");
+    let DateObject = new Date(Date.now());
+    file.writeFile(`\n${request.url} -- ${DateObject.getHours().toString()}:${DateObject.getMinutes().toString()}`);
+    response.end("data saved successfully");
+}
 
 server.listen(3000,"localhost",()=>console.log("server now is listening on port 3000"))
